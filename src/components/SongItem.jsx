b5cecd48331ee755790f4c51ee9e333109b279f7
Aditya Raj <aditya8raj000@gmail.com>
2022-06-15 23:33:35 +0530

    cards colors cahnged

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -5,7 +5,7 @@ export class SongItem extends Component {
         let { songName, artist, imgUrl, duration, likes } = this.props;
         return (
             <div>
-                <div className="card my-3" style={{ width: "12rem", color: "black" }}>
+                <div className="card my-3 text-bg-dark" style={{ width: "12rem", color: "black" }}>
                     <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h4><strong>{songName}</strong></h4>
